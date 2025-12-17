22f897c298e59bd66867ad0da4b12f4ab6dd59e7
Arshtyi <arshtyi@foxmail.com>
2025-12-17 17:04:58 +0800

    patch

--- update_banlist.py
+++ update_banlist.py
@@ -72,6 +72,10 @@ def get_card_id(card_name):
     """
     通过ygocdb.com获取卡片ID
     """
+    # 临时补丁: 替换全角尖括号为半角.https://mercury233.me/2022/01/27/%E7%99%BE%E9%B8%BD/
+    if card_name:
+        card_name = card_name.replace('＜', '<').replace('＞', '>')
+
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Accept': 'application/json',
