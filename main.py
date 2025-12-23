7a8aacc11212ee8e32058d2fcd782a1072ac7fd2
Arshtyi <arshtyi@foxmail.com>
2025-12-23 21:35:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.scrapers.master_duel import MasterDuelScraper
 from src.scrapers.ocg_tcg import OcgTcgScraper
 
 def main():
-   project_root = os.path.dirname(os.path.abspath(__file__))
+    project_root = os.path.dirname(os.path.abspath(__file__))
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
